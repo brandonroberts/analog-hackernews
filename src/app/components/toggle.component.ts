5310938521ba56836117d9376e5e1c665219b50c
Brandon Roberts <robertsbt@gmail.com>
2023-07-20 23:30:48 -0500

    refactor: show comments by default

--- src/app/components/toggle.component.ts
+++ src/app/components/toggle.component.ts
@@ -19,7 +19,7 @@ import { CommonModule } from '@angular/common';
   ]
 })
 export class ToggleComponent {
-  open = signal(false);
+  open = signal(true);
 
   toggle(state: boolean) {
     this.open.set(!state);
